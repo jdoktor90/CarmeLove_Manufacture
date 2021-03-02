b1a9498e70dbadc1bcdab26a596c968f1c77f075
Dorota Chroboczek <dorotachroboczek@gmail.com>
2021-03-02 21:24:26 +0100

    fix(cl): update func store because of Order & OrderItem class

--- store/views.py
+++ store/views.py
@@ -4,9 +4,17 @@ from .models import Customer, Category, Product, Order, OrderItem
 
 
 def store(request):
-    categories = Category.objects.all()
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cart_items = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
+        cart_items = order['get_cart_items']
     products = Product.objects.all()
-    context = {'categories': categories, 'products': products}
+    context = {'products': products, 'cart_items': cart_items}
     return render(request, 'store.html', context)
 
 
