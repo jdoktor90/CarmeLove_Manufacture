2aa838dd7a3dbd0c0d33a0c33c1f584f0a3d9d17
Dorota Chroboczek <dorotachroboczek@gmail.com>
2021-03-04 19:33:44 +0100

    feature(cl): list opinions of a product and detail view for product

--- store/urls.py
+++ store/urls.py
@@ -4,11 +4,12 @@ from .views import *
 
 urlpatterns = [
     #Leave as empty string for base url
-    path('', views.store, name="store"),
-    path('cart/', views.cart, name="cart"),
-    path('checkout/', views.checkout, name="checkout"),
-    path('update_item/', views.updateItem, name="update_item"),
-    path('process_order/', views.processOrder, name="process_order"),
+    path('', store, name="store"),
+    path('cart/', cart, name="cart"),
+    path('checkout/', checkout, name="checkout"),
+    path('update_item/', update_item, name="update_item"),
+    path('process_order/', process_order, name="process_order"),
+    path('<int:product_id>/product/', product, name="product"),
 
 ]
 
--- store/views.py
+++ store/views.py
@@ -1,9 +1,11 @@
 import datetime
 
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from django.http import JsonResponse
+from django.urls import reverse
+from django.contrib import messages
 
-from .models import Customer, Category, Product, Order, OrderItem
+from .models import Customer, Category, Product, Order, OrderItem, ProductOpinion
 
 
 def store(request):
@@ -34,6 +36,7 @@ def cart(request):
     context = {'items': items, 'order': order, 'cart_items': cart_items}
     return render(request, 'cart.html', context)
 
+
 def checkout(request):
     if request.user.is_authenticated:
         customer = request.user.customer
@@ -48,9 +51,11 @@ def checkout(request):
     context = {'items': items, 'order': order, 'cart_items': cart_items}
     return render(request, 'checkout.html', context)
 
+
 def update_item(request):
     return JsonResponse('Product was added', safe=False)
-  
+
+
 def process_order(request):
     transaction_id = datetime.datetime.now().timestamp()
     if request.user.is_authenticated:
@@ -59,4 +64,21 @@ def process_order(request):
     else:
         print('User is not logged in...')
     return JsonResponse('Payment submitted...', safe=False)
+
+
+def product(request, product_id):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cart_items = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
+        cart_items = order['get_cart_items']
+
+    viewed_product = Product.objects.get(id=product_id)
+    opinions = ProductOpinion.objects.filter(product=viewed_product)
+    context = {'product': viewed_product, 'opinions': opinions, 'cart_items': cart_items}
+    return render(request, 'product.html', context)
 
